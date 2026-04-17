0d84b04b3774b110c0bd089f26228831bfe82da5
sangramyadav05 <sangramyad05@gmail.com>
2026-04-17 17:51:31 +0530

    chore: reset project structure for new Flask-based system while preserving repo history

--- frontend/src/services/crowdSocket.js
+++
@@ -1,9 +0,0 @@
-import { io } from 'socket.io-client'
-
-const SOCKET_BASE_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000'
-
-export function createCrowdSocket() {
-  return io(SOCKET_BASE_URL, {
-    transports: ['websocket', 'polling'],
-  })
-}
